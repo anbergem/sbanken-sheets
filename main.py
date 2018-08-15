ee4c7a73cdf64440a35fba7db9b3669d164b5cda
Andreas Bergem <bergem.andreas@gmail.com>
2018-08-15 16:39:49 +0200

    Add Google Sheets integration
    Add basic functionality for appending values to a spreadsheet.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from pprint import pprint
 
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
 from Sbanken import Sbanken
 
 
-def main():
+def sbanken():
     import private_api_keys
 
     sbanken = Sbanken(private_api_keys.CLIENTID, private_api_keys.SECRET, private_api_keys.CUSTOMERID)
@@ -23,5 +27,51 @@ def main():
             file.write(f'{t.to_csv()}\n')
 
 
+def sheets():
+    SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+    import urls
+    # Call the Sheets API
+    SPREADSHEET_ID = urls.spreadsheet_id
+    # RANGE_NAME = ['Dummy!B5:E', 'Dummy!G5:J', 'Dummy!L5:O']
+    RANGE_NAME = 'Dummy!B5:E'
+    # result = service.spreadsheets().values().batchGet(spreadsheetId=SPREADSHEET_ID,
+    #                                                   ranges=RANGE_NAME).execute()
+    #
+    # for value_range in result['valueRanges']:
+    #     pprint(value_range)
+    #     values = value_range.get('values', [])
+    #
+    #     if not values:
+    #         print('No data found.')
+    #     else:
+    #         for row in values:
+                # Print columns A and E, which correspond to indices 0 and 4.
+                # print(','.join(row))
+
+    result = service.spreadsheets().values().append(
+        spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME,
+        valueInputOption='USER_ENTERED',
+        insertDataOption='INSERT_ROWS',
+        body={
+            'range': RANGE_NAME,
+            'values': [
+                ['En dato', '55', 'beskrivelse', 'Kategori?'],
+                ['12/10/2018', '129', 'Netflix', 'TV-abonnoment']
+            ]
+        }
+    ).execute()
+
+    pprint(result)
+
+
 if __name__ == "__main__":
-    main()
+    #sbanken()
+    sheets()
