e50eecd7a6b2f44f184df7426779ffce3e1585dd
Andreas Bergem <bergem.andreas@gmail.com>
2018-08-20 13:30:11 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sbankensheets',
+    version='0.0.1',
+    description='',
+    packages=['sbankensheets'],
+    license='MIT',
+    author='Andreas Bergem',
+    author_email='bergem.andreas@gmail.com',
+)
