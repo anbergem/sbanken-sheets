b81163874f24840b9d92507bcfc6394fc266885f
Andreas Bergem <bergem.andreas@gmail.com>
2018-08-20 22:48:02 +0200

    Change import style

--- sbankensheets/main.py
+++ sbankensheets/main.py
@@ -1,15 +1,14 @@
 from pprint import pprint
 
-from sbankensheets.sbanken import Sbanken, Transaction, divide_transactions, filter_transactions
-from sbankensheets.gsheets import GSheet, A1Range
+import sbankensheets.sbanken as sb
+import sbankensheets.gsheets as gs
 
 
 def main():
     from sbankensheets import private_api_keys
     from sbankensheets import urls
-    from sbankensheets.gsheets import google_sheets_helpers as gs
 
-    sbanken = Sbanken(private_api_keys.CLIENTID, private_api_keys.SECRET, private_api_keys.CUSTOMERID)
+    sbanken = sb.Sbanken(private_api_keys.CLIENTID, private_api_keys.SECRET, private_api_keys.CUSTOMERID)
 
     accounts = sbanken.get_accounts()
 
@@ -24,9 +23,9 @@ def main():
                                             start_date='2018-08-01',
                                             length=1000)
 
-    gsheets = GSheet(urls.spreadsheet_id)
+    gsheets = gs.GSheet(urls.spreadsheet_id)
 
-    divided_transactions = divide_transactions(transactions)
+    divided_transactions = sb.divide_transactions(transactions)
 
     # Todo: Include savings
     # Start cells
@@ -40,16 +39,16 @@ def main():
 
         # Extract encoding
         # + (0, 1) for dropping header
-        transaction_encodings_range = A1Range.from_cell(transaction_encoding_cell + (0, 1), sheet='Dummy')
+        transaction_encodings_range = gs.A1Range.from_cell(transaction_encoding_cell + (0, 1), sheet='Dummy')
 
         transaction_enc_values = gsheets.get(transaction_encodings_range)['values']
 
-        gs_transaction = map(lambda x: Transaction.decode(x[0]), filter(lambda x: not (x[0] == '.'),
+        gs_transaction = map(lambda x: sb.Transaction.decode(x[0]), filter(lambda x: not (x[0] == '.'),
                                                                         transaction_enc_values))
 
-        filtered_transactions = filter_transactions(divided_transactions[name], gs_transaction)
+        filtered_transactions = sb.filter_transactions(divided_transactions[name], gs_transaction)
 
-        transaction_range = A1Range.from_cell(transaction_encoding_cell, range=(4, 0), sheet='Dummy')
+        transaction_range = gs.A1Range.from_cell(transaction_encoding_cell, range=(4, 0), sheet='Dummy')
 
         # Append transactions
         values = list(map(lambda t: t.to_sheets_row(encode=True), filtered_transactions))
