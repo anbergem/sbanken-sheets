b357904d40b1e70ffc183f1c67c23cec799d238b
Andreas Bergem <bergem.andreas@gmail.com>
2018-08-23 21:05:51 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sbankensheets",
-    version="0.1.0",
+    version="0.2.0",
     description="",
     packages=["sbankensheets"],
     license="MIT",
